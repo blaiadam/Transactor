56c84a6393930a7f7de7b21a7a787dafec79ca1f
blaiadam <ablair44@live.com>
2025-06-19 11:05:34 -0500

    Incorporated SQL Server

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿// ***** Read Files *****
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
 using Transactor;
 
+// ***** Get List of Files from Directory *****
 if (args.Length != 1)
 {
     Console.WriteLine($"One argument is required");
@@ -16,7 +18,37 @@ if (transFiles.Length == 0)
     return;
 }
 
-List<Transaction> transactions = [];
+// ***** Setup SQL Connection *****
+// TODO: Need to update my service account.
+using SqlConnection conn = new(Globals.ConnectionString);
+conn.Open();
+using SqlCommand cmd = new(
+    @"INSERT INTO Transactions
+        (TransactionDate, 
+         PostedDate, 
+         CardNumber, 
+         Description, 
+         Category, 
+         Debit, 
+         Credit)
+      VALUES (@TransactionDate,
+              @PostedDate,
+              @CardNumber,
+              @Description,
+              @Category,
+              @Debit,
+              @Credit)",
+    conn
+);
+cmd.Parameters.Add("@TransactionDate", SqlDbType.Date);
+cmd.Parameters.Add("@PostedDate", SqlDbType.Date);
+cmd.Parameters.Add("@CardNumber", SqlDbType.NVarChar);
+cmd.Parameters.Add("@Description", SqlDbType.NVarChar);
+cmd.Parameters.Add("@Category", SqlDbType.NVarChar);
+cmd.Parameters.Add("@Debit", SqlDbType.Decimal);
+cmd.Parameters.Add("@Credit", SqlDbType.Decimal);
+
+// ***** Read Files *****
 string[] lineWords;
 string? line;
 DateTime transDate;
@@ -33,16 +65,19 @@ foreach (string transFile in transFiles)
         line = transFileReader.ReadLine();
         if (line is null)
             throw new Exception($"Null line in file");
+        if (line.Length == 0)
+            continue;
 
-        // TODO: The list's default capacity seems to be 100. Update this to use SQL lite or something else instead.
         lineWords = line.Split(',');
-        transactions.Add(new Transaction(DateTime.TryParse(lineWords[0], out transDate) ? transDate : default, // Transaction Date
-                                         DateTime.TryParse(lineWords[1], out postedDate) ? postedDate : default, // Posted Date
-                                         lineWords[2], // Card Number
-                                         lineWords[3], // Description
-                                         lineWords[4], // Category
-                                         decimal.TryParse(lineWords[5], out debit) ? debit : 0, // Debit
-                                         decimal.TryParse(lineWords[6], out credit) ? credit : 0)); // Credit
+
+        cmd.Parameters["@TransactionDate"].Value = DateTime.TryParse(lineWords[0], out transDate) ? transDate : default;
+        cmd.Parameters["@PostedDate"].Value = DateTime.TryParse(lineWords[1], out postedDate) ? postedDate : default;
+        cmd.Parameters["@CardNumber"].Value = lineWords[2];
+        cmd.Parameters["@Description"].Value = lineWords[3];
+        cmd.Parameters["@Category"].Value = lineWords[4];
+        cmd.Parameters["@Debit"].Value = decimal.TryParse(lineWords[5], out debit) ? debit : 0;
+        cmd.Parameters["@Credit"].Value = decimal.TryParse(lineWords[6], out credit) ? credit : 0;
+        cmd.ExecuteNonQuery();
     }
     transFileReader.Close();
 }
@@ -54,7 +89,7 @@ Console.WriteLine();
 string? userInput = "";
 while (userInput != "exit")
 {
-    Console.WriteLine("How would you like to view your transactions? Options: [list]");
+    Console.WriteLine("How would you like to view your transactions? Options: [list] | [exit] to exit the program");
     userInput = Console.ReadLine();
 
     switch (userInput)
