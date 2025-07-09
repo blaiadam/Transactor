b63a873b72b16198808301ebd02f32556e98fda1
blaiadam <ablair44@live.com>
2025-07-08 20:26:27 -0500

    Added comments, updated the file controller read to not use arguments, fixed the padright for transaction printing

--- Transaction.cs
+++ Transaction.cs
@@ -19,16 +19,16 @@ public class Transaction(DateTime transactionDate,
     #endregion
 
     #region Private Properties
-    private readonly short TransactionDateColWidth = 20;
-    private readonly short PostedColWidth = 20;
-    private readonly short CardNumberColWidth = 15;
-    private readonly short DescriptionColWidth = 50;
-    private readonly short CategoryColWidth = 30;
-    private readonly short DebitColWidth = 10;
-    private readonly short CreditColWidth = 10;
-    private readonly string TransactionDateHeader = "Transaction Date";
+    private readonly short TransactionDateColWidth = 15;
+    private readonly short PostedColWidth = 15;
+    private readonly short CardNumberColWidth = 5;
+    private readonly short DescriptionColWidth = 30;
+    private readonly short CategoryColWidth = 20;
+    private readonly short DebitColWidth = 8;
+    private readonly short CreditColWidth = 8;
+    private readonly string TransactionDateHeader = "Transaction";
     private readonly string PostedHeader = "Posted";
-    private readonly string CardNumberHeader = "Card Number";
+    private readonly string CardNumberHeader = "Card";
     private readonly string DescriptionHeader = "Description";
     private readonly string CategoryHeader = "Category";
     private readonly string DebitHeader = "Debit";
@@ -38,24 +38,24 @@ public class Transaction(DateTime transactionDate,
     #region Public Methods
     public void PrintHeaders()
     {
-        Console.Write($"{TransactionDateHeader.PadRight(TransactionDateColWidth - TransactionDateHeader.Length)}");
-        Console.Write($"{PostedHeader.PadRight(PostedColWidth - PostedHeader.Length)}");
-        Console.Write($"{CardNumberHeader.PadRight(CardNumberColWidth - CardNumberHeader.Length)}");
-        Console.Write($"{DescriptionHeader.PadRight(DescriptionColWidth - DescriptionHeader.Length)}");
-        Console.Write($"{CategoryHeader.PadRight(CategoryColWidth - CategoryHeader.Length)}");
-        Console.Write($"{DebitHeader.PadRight(DebitColWidth - DebitHeader.Length)}");
-        Console.Write($"{CreditHeader.PadRight(CreditColWidth - CreditHeader.Length)}{Environment.NewLine}");
+        Console.WriteLine($"{TransactionDateHeader.PadRight(TransactionDateColWidth)}" +
+                          $"{PostedHeader.PadRight(PostedColWidth)}" +
+                          $"{CardNumberHeader.PadRight(CardNumberColWidth)}" +
+                          $"{DescriptionHeader.PadRight(DescriptionColWidth)}" +
+                          $"{CategoryHeader.PadRight(CategoryColWidth)}" +
+                          $"{DebitHeader.PadRight(DebitColWidth)}" +
+                          $"{CreditHeader.PadRight(CreditColWidth)}");
     }
 
     public void Print()
     {
-        Console.Write($"{TransactionDate.ToShortDateString().PadRight(TransactionDateColWidth - TransactionDate.ToShortDateString().Length)}");
-        Console.Write($"{Posted.ToShortDateString().PadRight(PostedColWidth - Posted.ToShortDateString().Length)}");
-        Console.Write($"{CardNumber.PadRight(CardNumberColWidth - CardNumber.Length)}");
-        Console.Write($"{Description.PadRight(DescriptionColWidth - Description.Length)}");
-        Console.Write($"{Category.PadRight(CategoryColWidth - Category.Length)}");
-        Console.Write($"{Debit.ToString().PadRight(DebitColWidth - Debit.ToString().Length)}");
-        Console.Write($"{Credit.ToString().PadRight(CreditColWidth - Debit.ToString().Length)}{Environment.NewLine}");
+        Console.WriteLine($"{TransactionDate.ToShortDateString().PadRight(TransactionDateColWidth)}" +
+                          $"{Posted.ToShortDateString().PadRight(PostedColWidth)}" + 
+                          $"{CardNumber.PadRight(CardNumberColWidth)}" +
+                          $"{Description.PadRight(DescriptionColWidth)}" +
+                          $"{Category.PadRight(CategoryColWidth)}" +
+                          $"{Debit.ToString().PadRight(DebitColWidth)}" +
+                          $"{Credit.ToString().PadRight(CreditColWidth)}");
     }
     #endregion
 }
